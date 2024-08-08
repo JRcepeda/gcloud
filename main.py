f3593595a00701618c9a66cc672b5776bdcf8a09
JRcepeda <josercepeda01@gmail.com>
2024-08-08 01:36:06 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import jsonify
+#import os
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a frinedly HTTP greeting."""
+    print("I am inside hello world")
+    return "Hello world"
+
+@app.route('/echo/<name>')
+def echo(name):
+    print(f"This was placed in the url: new-{name}")
+    val={"new-name":name}
+    return jsonify(val)
+
+if __name__=='__main__':
+    app.run(debug=True)
+   # app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
