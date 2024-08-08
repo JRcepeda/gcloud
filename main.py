66c77a36c30e39448ba6665eeb9178b7c0de3f28
JRcepeda <josercepeda01@gmail.com>
2024-08-08 03:01:48 +0000

    Last Deploy

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ def echo(name):
     return jsonify(val)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1',port=8080,debug=True)
    # app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
